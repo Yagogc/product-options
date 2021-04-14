baec6b7a5f5998150a1e222d1f0792b93871dcd6
Yago Gonzalez <jagogc@gmail.com>
2021-04-14 22:51:00 +0100

    Update ProductCard

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,13 @@
-const ProductCard = () => {
-  return <div>Product Card</div>
+import { Product } from '../data/products.types'
+
+const ProductCard = ({ skus }: { skus: Product[] }) => {
+  return (
+    <div>
+      {skus.map((sku, i) => (
+        <pre key={i}>{JSON.stringify(sku, null, 2)}</pre>
+      ))}
+    </div>
+  )
 }
 
 export { ProductCard }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 import { ProductCard } from '../components/ProductCard'
+import data from '../data/products.json'
+import { groupByGroupId } from '../utils/groupByGroupId'
+
+const products = groupByGroupId(data.products)
 
 export default function Home() {
-  return <ProductCard />
+  return (
+    <>
+      {Object.entries(products).map(([key, skus]) => (
+        <ProductCard key={key} skus={skus} />
+      ))}
+    </>
+  )
 }
