5ce859fe4024fe098f6aefd2af7a62874be625f7
Yago Gonzalez <jagogc@gmail.com>
2021-04-14 22:33:44 +0100

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
