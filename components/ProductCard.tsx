5f3597435de1ccaf41c5ee4357923fbb85ba3359
Yago Gonzalez <jagogc@gmail.com>
2021-04-14 23:36:49 +0100

    Add ProductCard logic

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import { ChangeEvent, useEffect, useMemo, useState } from 'react'
 
 import { Attribute, AttrType, Product } from '../data/products.types'
 import { getUniqueAttrs } from '../utils/getUniqueAttrs'
@@ -17,21 +17,48 @@ import {
 
 const ProductCard = ({ skus }: { skus: Product[] }) => {
   const uniqueAttr = useMemo(() => getUniqueAttrs(skus), [skus])
+  const [selectedSku, setSelectedSku] = useState<Product | undefined>(skus[0])
+  const [selectedAttrs, setSelectedAttrs] = useState<
+    {
+      [key in AttrType]: string
+    }
+  >({
+    [AttrType.colour]: uniqueAttr[AttrType.colour][0].id,
+    [AttrType.type]: uniqueAttr[AttrType.type][0].id,
+    [AttrType.case]: uniqueAttr[AttrType.case][0].id,
+  })
+
+  useEffect(() => {
+    const selected = skus.find(
+      (sku) =>
+        sku.attributes.filter((attr) => attr.id === selectedAttrs[attr.type])
+          .length === 3
+    )
+    setSelectedSku(selected)
+  }, [skus, selectedAttrs, setSelectedSku])
 
   return (
     <Card>
       <Img src="./placeholder.png" alt="Product" />
       <ProductInfo>
-        <h1>Title</h1>
-        <p>description</p>
+        <h1>{selectedSku?.['product-group-label']}</h1>
+        <p>{selectedSku?.['item-description']}</p>
         <VariantsContainer>
           {Object.entries(uniqueAttr).map(([key, attrs]) => {
-            return <AttributeSwatch key={key} type={key} attrs={attrs} />
+            return (
+              <AttributeSwatch
+                key={key}
+                type={key}
+                attrs={attrs}
+                selectedAttr={selectedAttrs}
+                setSelectedAttrs={setSelectedAttrs}
+              />
+            )
           })}
         </VariantsContainer>
         <Summary>
           <TotalPrice>
-            Total <Price>price</Price>
+            Total <Price>${selectedSku?.price.toFixed(2)}</Price>
           </TotalPrice>
           <Button type="button">Add to basket</Button>
         </Summary>
@@ -43,17 +70,33 @@ const ProductCard = ({ skus }: { skus: Product[] }) => {
 const AttributeSwatch = ({
   type,
   attrs,
+  selectedAttr,
+  setSelectedAttrs,
 }: {
   type: string
   attrs: Attribute[]
+  selectedAttr: { [key: string]: string }
+  setSelectedAttrs: Function
 }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newSelectedAttrs = { ...selectedAttr, [type]: e.target.value }
+    setSelectedAttrs(newSelectedAttrs)
+  }
+
   if (type === AttrType.colour) {
     return (
       <VariantBlock>
         <div>Colour:</div>
         {attrs.map((attr) => (
           <Label key={attr.id} htmlFor={attr.id}>
-            <input type="radio" id={attr.id} value={attr.id} name={type} />
+            <input
+              type="radio"
+              id={attr.id}
+              value={attr.id}
+              name={type}
+              checked={attr.id === selectedAttr[type]}
+              onChange={handleChange}
+            />
             <span>{attr.label}</span>
           </Label>
         ))}
@@ -66,7 +109,14 @@ const AttributeSwatch = ({
         <div>Paper type:</div>
         {attrs.map((attr) => (
           <Label key={attr.id} htmlFor={attr.id}>
-            <input type="radio" id={attr.id} value={attr.id} name={type} />
+            <input
+              type="radio"
+              id={attr.id}
+              value={attr.id}
+              name={type}
+              checked={attr.id === selectedAttr[type]}
+              onChange={handleChange}
+            />
             <div>{attr.label}</div>
           </Label>
         ))}
@@ -79,7 +129,14 @@ const AttributeSwatch = ({
         <div>Slipcase:</div>
         {attrs.map((attr) => (
           <Label key={attr.id} htmlFor={attr.id}>
-            <input type="radio" id={attr.id} value={attr.id} name={type} />
+            <input
+              type="radio"
+              id={attr.id}
+              value={attr.id}
+              name={type}
+              checked={attr.id === selectedAttr[type]}
+              onChange={handleChange}
+            />
             <span>{attr.value ? 'Yes' : 'No'}</span>
           </Label>
         ))}
