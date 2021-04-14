b5adf4a51a011a0c74f05f78aaba2e8d6cff7c00
Yago Gonzalez <jagogc@gmail.com>
2021-04-14 23:00:54 +0100

    Add ProductCard styles

---
+++ components/ProductCard.css.ts
@@ -0,0 +1,73 @@
+import { styled } from '../styles/config'
+
+const Card = styled('div', {
+  background: 'white',
+  borderRadius: '5px',
+  padding: '20px',
+  borderWidth: '4px',
+  borderStyle: 'solid',
+  borderColor: '$neutral',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  '@bp2': {
+    flexDirection: 'row',
+  },
+})
+
+const ProductInfo = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+  gap: '10px',
+})
+
+const Img = styled('img', {
+  margin: '0 auto',
+  height: '150px',
+  width: '150px',
+})
+
+const Summary = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginTop: '10px',
+  gap: '10px',
+})
+
+const TotalPrice = styled('span', {
+  background: '$neutral',
+  borderRadius: '2px',
+  padding: '2px',
+  paddingLeft: '5px',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  color: 'white',
+  textTransform: 'uppercase',
+  fontWeight: 'bold',
+})
+
+const Price = styled('span', {
+  fontVariantNumeric: 'tabular-nums',
+  background: 'white',
+  borderRadius: '1px',
+  padding: '4px 6px',
+  color: 'black',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+})
+
+const Button = styled('button', {
+  background: '$neutral',
+  fontWeight: 'bold',
+  border: 'none',
+  padding: '5px 10px',
+  borderRadius: '2px',
+  color: 'white',
+  cursor: 'pointer',
+})
+
+export { Button, Card, Img, Price, ProductInfo, Summary, TotalPrice }
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,12 +1,30 @@
 import { Product } from '../data/products.types'
+import {
+  Button,
+  Card,
+  Img,
+  Price,
+  ProductInfo,
+  Summary,
+  TotalPrice,
+} from './ProductCard.css'
 
 const ProductCard = ({ skus }: { skus: Product[] }) => {
   return (
-    <div>
-      {skus.map((sku, i) => (
-        <pre key={i}>{JSON.stringify(sku, null, 2)}</pre>
-      ))}
-    </div>
+    <Card>
+      <Img src="./placeholder.png" alt="Product" />
+      <ProductInfo>
+        <h1>Title</h1>
+        <p>description</p>
+        <div>Variants</div>
+        <Summary>
+          <TotalPrice>
+            Total <Price>price</Price>
+          </TotalPrice>
+          <Button type="button">Add to basket</Button>
+        </Summary>
+      </ProductInfo>
+    </Card>
   )
 }
 
